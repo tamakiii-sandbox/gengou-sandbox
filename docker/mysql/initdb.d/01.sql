79e306849d4af273ac33196859fc9a2daafc8a8e
tamakiii <tamakiii@users.noreply.github.com>
2019-04-02 01:31:00 +0900

    fix table collate

--- docker/mysql/initdb.d/01.sql
+++ docker/mysql/initdb.d/01.sql
@@ -3,4 +3,4 @@ CREATE TABLE `test` (
   `string` varchar(8) NOT NULL,
   `unicode` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_cs;
